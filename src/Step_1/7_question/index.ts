d93f6cb6bd7a73807cdbb6dbde86d492b661c214
ByeongHunKim <rkwhr912@naver.com>
2023-01-25 20:53:36 +0900

    :white_check_mark: [1단계-문제7] 완료
    https://www.acmicpc.net/problem/10926

--- src/Step_1/7_question/index.ts
+++ src/Step_1/7_question/index.ts
@@ -1,6 +1,6 @@
 import fs = require('fs')
 const inputFile = 'example.txt'
-let input = fs.readFileSync('/dev/stdin').toString()
-// let input = fs.readFileSync(inputFile).toString()
+let input = fs.readFileSync('/dev/stdin').toString().trim();
+// let input = fs.readFileSync(inputFile).toString().trim();
 // let result = input + '??!'
 console.log(input + '??!')
