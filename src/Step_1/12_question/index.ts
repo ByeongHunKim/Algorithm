4a19413f202e06f73b8a0524b84e77f7a1e07644
ByeongHunKim <rkwhr912@naver.com>
2023-01-27 13:47:59 +0900

    :construction: [1단계-문제12] 진행중
    https://www.acmicpc.net/problem/10171

--- src/Step_1/12_question/index.ts
+++ src/Step_1/12_question/index.ts
@@ -1,2 +1,12 @@
-const first = '\/\'
-console.log(`${first}`)
+let first = ` \\    /\\`
+let second = `  )  ( ')`
+let third = ` (  /  )`
+let fourth = `  \\(__)|`
+console.log(`${first}\n${second}\n${third}\n${fourth}`)
+
+/*
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+*/
