a64e4da3cb677281ce04153c5570f59573f25e24
ByeongHunKim <rkwhr912@naver.com>
2023-01-31 15:53:50 +0900

    :white_check_mark: [2단계-문제4] 완료
    https://www.acmicpc.net/problem/14681

--- src/Step_2/4_question/index.ts
+++ src/Step_2/4_question/index.ts
@@ -1,41 +1,19 @@
 import fs = require('fs')
 const inputfile = 'example.txt'
-// let input = fs.readFileSync(inputfile).toString().split('\n');
-// let x = parseInt((input[0]));
-// let y = parseInt((input[1]));
+const [x, y] = fs.readFileSync(inputfile).toString().trim().split('\n').map(Number);
 
-const [A, B] = fs.readFileSync(inputfile).toString().trim().split('\n');
+if(x > 0) y > 0 ? console.log(1) : console.log(4)
+if(x < 0) y > 0 ? console.log(2) : console.log(3)
 
-let x = parseInt([A][0])
-let y = parseInt([B][0])
-
-if(x > 0 && y > 0){
-    console.log(1)
-}else if( x < 0 && y > 0){
-    console.log(2)
-}else if ( x > 0 && y < 0){
-    console.log(3)
-}else{
-    console.log(4)
-}
 
 
 
 
 /*
 import fs = require('fs')
-let [A, B] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-let x = parseInt([A][0])
-let y = parseInt([B][0])
+const [x, y] = fs.readFileSync(0).toString().trim().split('\n').map(Number)
+if(x > 0) y > 0 ? console.log(1) : console.log(4)
+if(x < 0) y > 0 ? console.log(2) : console.log(3)
 
-if(x > 0 && y > 0){
-    console.log(1)
-}else if( x < 0 && y > 0){
-    console.log(2)
-}else if ( x > 0 && y < 0){
-    console.log(3)
-}else{
-    console.log(4)
-}
 */
 
