3e1b23bb3574142238ba85f0746d34cbadafb01a
ByeongHunKim <rkwhr912@naver.com>
2023-01-31 13:49:46 +0900

    :construction: [2단계-문제4] 진행중
    https://www.acmicpc.net/problem/14681

--- src/Step_2/4_question/index.ts
+++ src/Step_2/4_question/index.ts
@@ -1,13 +1,13 @@
 import fs = require('fs')
 const inputfile = 'example.txt'
-let input = fs.readFileSync(inputfile).toString().split(' ');
-let x = parseInt((input[0]));
-let y = parseInt((input[1]));
+// let input = fs.readFileSync(inputfile).toString().split('\n');
+// let x = parseInt((input[0]));
+// let y = parseInt((input[1]));
 
-// case1: x,y 둘다 양수인 경우
-// case2: x 양수 y 음수인 경우
-// case3: x 음수 y 양수인 경우
-// case4: x,y 둘다 음수인 경우
+const [A, B] = fs.readFileSync(inputfile).toString().trim().split('\n');
+
+let x = parseInt([A][0])
+let y = parseInt([B][0])
 
 if(x > 0 && y > 0){
     console.log(1)
@@ -19,11 +19,14 @@ if(x > 0 && y > 0){
     console.log(4)
 }
 
+
+
+
 /*
 import fs = require('fs')
-let input = fs.readFileSync('/dev/stdin').toString().split(' ');
-let x = parseInt((input[0]));
-let y = parseInt((input[1]));
+let [A, B] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+let x = parseInt([A][0])
+let y = parseInt([B][0])
 
 if(x > 0 && y > 0){
     console.log(1)
@@ -35,3 +38,4 @@ if(x > 0 && y > 0){
     console.log(4)
 }
 */
+
