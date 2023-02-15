ecfce05c63866297955875e669ff3fd0fd7167ec
ByeongHunKim <rkwhr912@naver.com>
2023-02-15 22:56:54 +0900

    :white_check_mark: [2단계-문제6] 완료
    https://www.acmicpc.net/problem/2525

--- src/Step_2/5_question/index.ts
+++ src/Step_2/5_question/index.ts
@@ -1,6 +1,11 @@
-import fs = require('fs')
-const inputfile = 'example.txt'
-const [x, y] = fs.readFileSync(inputfile).toString().trim().split(' ').map(Number);
+import fs = require("fs");
+const inputfile = "example.txt";
+const [x, y] = fs
+  .readFileSync(inputfile)
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
 
 /*
 두 가지 경우의 수
@@ -13,22 +18,22 @@ hour값을 1 빼준다.
 만약 1을 뺀 값이 -1일 경우 hour값은 23이 된다
 */
 
-let hour = x
-let min = y - 45
+let hour = x;
+let min = y - 45;
 
-console.log('min:', min)
+console.log("min:", min);
 
-if(min < 0){
-    // console.log('분이 0보다 작습니다')
-    min += 60
-    hour -= 1
-    if(hour == -1){
-        // console.log('시간이 0보다 작습니다')
-        hour = 23
-    }
-    // console.log(hour, min)
+if (min < 0) {
+  // console.log('분이 0보다 작습니다')
+  min += 60;
+  hour -= 1;
+  if (hour == -1) {
+    // console.log('시간이 0보다 작습니다')
+    hour = 23;
+  }
+  // console.log(hour, min)
 }
-console.log(hour, min)
+console.log(hour, min);
 /*
 import fs = require('fs')
 const [x, y] = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
--- src/Step_2/6_question/index.ts
+++ src/Step_2/6_question/index.ts
@@ -1,6 +1,17 @@
 import fs = require("fs");
-const inputfile = "example.txt";
-console.log(inputfile);
-const [x, y] = fs.readFileSync(inputfile).toString().split(" ");
+const file = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+let inputFile = fs.readFileSync(file).toString().split("\n");
 
-console.log("x, y", x, y);
+let [curHour, curMin] = inputFile[0].split(" ").map(Number);
+let cookTime = +inputFile[1];
+
+curMin += cookTime;
+
+while (curMin >= 60) {
+  curMin -= 60;
+  curHour += 1;
+}
+
+curHour %= 24;
+
+console.log(curHour, curMin);
