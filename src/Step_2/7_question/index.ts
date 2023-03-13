15e2d8cfc0612e2703ce3fc22dd6650ce8a17191
ByeongHunKim <rkwhr912@naver.com>
2023-03-13 23:51:21 +0900

    :white_check_mark: [2단계-문제7] 완료
    https://overcome-the-limits.tistory.com/entry/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EB%B0%B1%EC%A4%80-2480-%EC%A3%BC%EC%82%AC%EC%9C%84-%EC%84%B8%EA%B0%9C-with-nodejs

--- src/Step_2/7_question/index.ts
+++ src/Step_2/7_question/index.ts
@@ -2,22 +2,39 @@ import fs = require('fs');
 const file = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
 let inputFile = fs.readFileSync(file).toString().split(' ').map(Number);
 
-// todo 1 : 입력된 값을 하나 씩 접근해둔다
+// todo 1 : 입력된 값을 하나 씩 접근해둔다 [완료]
 const [a, b, c] = [inputFile[0], inputFile[1], inputFile[2]];
 
 let biggest_num = 0;
 let result = 0;
 
-console.log('1. numbers : ', a, b, c, inputFile);
-
 const answer = (a, b, c): Number => {
-  // todo 2 : 입력된 값이 모두 같은 경우
-  console.log('abc값은?', a, b, c);
+  // todo 2 : 입력된 값이 모두 같은 경우 [완료]
   if (a === b && a === c && b === c) {
+    console.log(10000 + a * 1000);
     result = 10000 + a * 1000;
     return result;
   }
-  // todo 3 : 입력된 값들이 서로 다른 경우 => 1000 + 3개의 수 중 가장 큰 수 * 100
+  // todo 3 : 입력된 값들이 서로 다른 경우 => 1000 + 3개의 수 중 가장 큰 수 * 100 [완료]
+  if (a !== b || a !== c || b !== c) {
+    if (a === b && a === c) {
+      console.log(1000 + a * 100);
+      result = 1000 + a * 100;
+      return result;
+    }
+    if (b === c) {
+      console.log(1000 + b * 100);
+      result = 1000 + b * 100;
+      return result;
+    }
+  }
+  // todo 4 : 모두 다른 경우엔 가장 큰 수 * 100 [완료]
+  if (a !== b && a !== c && b !== c) {
+    const sort = [a, b, c].sort();
+    console.log(sort.pop() * 100);
+    result = sort.pop() * 100;
+    return result;
+  }
 };
 
 console.log('주사위를 던진 결과는?', answer(a, b, c));
