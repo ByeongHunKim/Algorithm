ad2929d2774f23a8ea0df71e746bb12510c15ea9
ByeongHunKim <rkwhr912@naver.com>
2023-04-01 20:48:24 +0900

    🚧 [4단계-문제3] 진행중
    https://www.acmicpc.net/problem/10818

---
+++ src/Step_4/3_question/index.ts
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
+let input = fs.readFileSync(file).toString().split('\n');
+
+const count = Number(input[0]) - 1;
+const numList = input[1].split(' ');
+
+let minNum = Number(numList[0]);
+let maxNum = 0;
+
+for(let i = 1; i <= count; i++){
+    let compareNum = Number(numList[i]);
+    if(compareNum < minNum){
+        minNum = compareNum;
+    } else if(compareNum > maxNum) {
+        maxNum = compareNum;
+    }
+}
+console.log(`${minNum} ${maxNum}`)
